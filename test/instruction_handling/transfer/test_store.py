5a38993a8bdf1b84c63bf316700d808bad37084d
ashli <ashlig@protonmail.com>
2025-04-19 14:46:06 +0200

    refactoring wip

--- test/instruction_handling/transfer/test_store.py
+++ test/instruction_handling/transfer/test_store.py
@@ -1,75 +1,64 @@
 import unittest
 import random
+from abc import ABC, abstractmethod
+from functools import reduce
+
 from cpu import CPU
 from memory import Memory
 
-class StoreCommon(unittest.TestCase):
+class StoreCommon(ABC, unittest.TestCase):
     def setUp(self):
         self.memory = Memory()
         self.cpu = CPU(memory=self.memory, test=True)
         self.cpu_state_init = self.cpu.state
         self.cpu.reset()
+    
+    def _run_test_cycles(self, opcode: int, r_from: str, expected_cycles: int, **kwargs):
+        self.cpu.reset_state()
+        self.cpu.state.set_by_id("pc", 0x1000)
+        self.memory.set_bytes(0x1000, [opcode, 0x10])
+        state = self.cpu.run()
+        self.assertEqual(state.get_by_id("clock_cycles"), expected_cycles)
+    
+    def _run_test_transfer(self, opcode: int, r_from: str, val: int, addr: int, expected_addr: int, expected_val: int, **kwargs):
+        self.cpu.reset_state()
+        self.cpu.state.set_by_id("pc", 0x1000)
+        self.memory.set_bytes(0x1000, [opcode, addr])
+        self.cpu.state.set_by_id(r_from, val)
+        state = self.cpu.run()
+        self.assertEqual(expected_val, self.memory.get_byte(expected_addr))
+
 
 class TestStoreInstructionsZP(StoreCommon):
-    # ZP
-    def _gen_test_params(self):
-        return (
-            (0x86, "X"),
-            (0x84, "Y"),
-            (0x85, "A")
-        )
+    def _gen_test_params(self, exclude=[]):
+        return [
+            {"opcode": 0x86, "r_from": "X"},
+            {"opcode": 0x84, "r_from": "Y"},
+            {"opcode": 0x85, "r_from": "A"}
+        ]
 
     def test_all_cycles(self):
-        def run_test(opcode: int):
-            self.cpu.state.set_by_id("pc", 0x1000)
-            self.memory.set_bytes(0x1000, [opcode, 0x10])
-            state = self.cpu.run()
-            self.assertEqual(state.get_by_id("clock_cycles"), 3)
-        
         for test in self._gen_test_params():
-            run_test(test[0])
+            self._run_test_cycles(**test, expected_cycles=3)
     
     def test_all_transfer(self):
-        def run_test(opcode: int, r_from: str, val: int, addr: int):
-            self.cpu.state.set_by_id("pc", 0x1000)
-            self.memory.set_bytes(0x1000, [opcode, addr])
-            self.cpu.state.set_by_id(r_from, val)
-            state = self.cpu.run()
-            self.assertEqual(val, self.memory.get_byte(addr))
-
         for test in self._gen_test_params():
-            run_test(*test, val=random.randint(0, 255), addr=random.randint(0, 255))
+            addr = random.randint(0, 255)
+            val = random.randint(0, 255)
+            self._run_test_transfer(**test, val=val, addr=addr, expected_addr=addr, expected_val=val)
 
-class TestStoreInstructionsZPI(StoreCommon):
+class TestStoreInstructionsZPI(TestStoreInstructionsZP):
     def _gen_test_params(self):
-        return (
-            (0x96, "X", "Y"),
-            (0x94, "Y", "X"),
-            (0x95, "A", "X")
-        )
+        return [
+            {"opcode": 0x96, "r_from": "X", "r_index": "Y"},
+            {"opcode": 0x94, "r_from": "Y", "r_index": "X"},
+            {"opcode": 0x95, "r_from": "A", "r_index": "X"}
+        ]
     
     def test_all_cycles(self):
-        def run_test(opcode: int):
-            self.cpu.state.set_by_id("pc", 0x1000)
-            self.memory.set_bytes(0x1000, [opcode, 0x10])
-            state = self.cpu.run()
-            self.assertEqual(state.get_by_id("clock_cycles"), 4)
-        
         for test in self._gen_test_params():
-            run_test(test[0])
-
-    def test_all_transfer(self):
-        def run_test(opcode: int, r_from: str, r_index: str, val: int, addr: int):
-            self.cpu.state.set_by_id("pc", 0x1000)
-            self.memory.set_bytes(0x1000, [opcode, addr])
-            self.cpu.state.set_by_id(r_from, val)
-            state = self.cpu.run()
-            self.assertEqual(self.cpu.memory.get_byte(addr), val)
-        
-        for test in self._gen_test_params():
-            self.cpu.reset_state()
-            run_test(*test, val=random.randint(0, 255), addr=random.randint(0, 255))
-
+            self._run_test_cycles(**test, expected_cycles=4)
+'''
     def test_all_transfer_indexed(self):
         def run_test(opcode: int, r_from: str, r_index: str, val: int, addr: int, index: int):
             self.cpu.reset_state()
@@ -105,7 +94,7 @@ class TestStoreInstructionsZPI(StoreCommon):
             run_test(*test, val=0xFF, addr=0x00, index=0x00)
             run_test(*test, val=0xFF, addr=0xFF, index=0x00)
             run_test(*test, val=0xFF, addr=0x00, index=0xFF)
-            run_test(*test, val=0xFF, addr=0xFF, index=0xFF)
+            run_test(*test, val=0xFF, addr=0xFF, index=0xFF)'''
 # ABS TODO
 # ABI TODO
 # IND TODO
